0c5ddc8e8404cc0cde3dfd13194896667cb722c6
Kumar Vikram <vikram2015cse01@gmail.com>
2023-10-25 09:23:16 +0530

    Converter completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { IoGitCompare } from "react-icons/io5";
+import useCurrencyInfo from './hooks/useCurrencyInfo';
+import {InputComponent} from './components';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isSwap, setIsSwap] = useState(false);
+  const [fromCurrency, setFromCurrency] = useState("usd");
+  const [toCurrency, setToCurrency] = useState("inr");
+  const [amount, setAmount] = useState(1);
+  const [toAmount, setToAmount] = useState(0);
 
+  const handleSwap = () =>{
+    setIsSwap(!isSwap);
+    setFromCurrency(toCurrency);
+    setToCurrency(fromCurrency);
+    setAmount(Number(toAmount));
+  }
+  
+  const currencyInfo = useCurrencyInfo(fromCurrency);
+  const options = Object.keys(currencyInfo);
+  
+  const handleFromAmount=(e) =>{
+    setAmount(Number(e.target.value));
+  }
+
+  const handleToAmount = (e)=>{
+    setToAmount(Number(e.target.value));
+  }
+  useEffect(() => {
+    console.log("Hi", currencyInfo[toCurrency]);
+    if (currencyInfo[toCurrency] !== undefined && amount !== undefined) {
+      setToAmount(amount * currencyInfo[toCurrency]);
+    }
+  }, [amount, toCurrency, currencyInfo]);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen bg-black flex flex-col justify-center items-center">
+      <h1 className="text-4xl text-slate-700 my-8">Currency Convertor</h1>
+      <div className="w-1/3 bg-slate-500 shadow-2xl rounded p-8 flex flex-col justify-center items-center">
+        <InputComponent
+          label="From"
+          amount={amount}
+          currency={fromCurrency}
+          handleAmount={handleFromAmount}
+          handleCurrency={setFromCurrency}
+          options={options}
+        />
+        <div
+          className="bg-blue-700 inline-block p-2 px-5 -mt-2 z-30 rounded-md  cursor-pointer"
+          onClick={handleSwap}
+        >
+          <IoGitCompare className="text-slate-200" />
+        </div>
+        <InputComponent
+          label="To"
+          amount={toAmount}
+          currency={toCurrency}
+          handleAmount={handleToAmount}
+          handleCurrency={setToCurrency}
+          options={options}
+          className="-mt-2"
+          isInputDisable={true}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
---
+++ src/components/InputComponent.jsx
@@ -0,0 +1,65 @@
+import PropTypes from "prop-types";
+const InputComponent = ({
+  label,
+  amount = 1,
+  currency = "usd",
+  handleAmount,
+  handleCurrency,
+  isInputDisable,
+  options,
+  isCurrencyDisabled,
+  className
+}) => {
+  return (
+    <div className={`bg-slate-200 rounded-sm flex w-full ${className}`}>
+      <div className="flex-grow ps-4 pt-1">
+        <h1 className="text-slate-500 tracking-wider">{label}</h1>
+        <input
+          type="number"
+          value={amount}
+          onChange={(e) => handleAmount(e)}
+          name="sourceAmount"
+          disabled={isInputDisable}
+          className="my-3 text-blue-700 outline-none bg-slate-200 font-semibold tracking-wide w-full"
+        />
+      </div>
+      <div className="text-right pe-4 pt-1">
+        <h1 className="text-slate-500 tracking-wider">Currency Type</h1>
+        <select
+          type="text"
+          name="currency"
+          placeholder="0"
+          className="my-3 text-blue-700 outline-none bg-slate-300 font-semibold rounded-sm p-2"
+          value={currency}
+          onChange={(e) => handleCurrency(e.target.value)}
+          disabled={isCurrencyDisabled}
+        >
+          {options &&
+            options.map((currency) => (
+              <option key={currency} value={currency}>
+                {currency}
+              </option>
+            ))}
+        </select>
+      </div>
+    </div>
+  );
+};
+
+InputComponent.defaultProps = {
+  isInputDisable: false,
+  isCurrencyDisabled: false
+};
+InputComponent.propTypes = {
+  label: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+  currency: PropTypes.string.isRequired,
+  handleAmount: PropTypes.func.isRequired,
+  handleCurrency: PropTypes.func.isRequired,
+  isInputDisable: PropTypes.bool.isRequired,
+  options: PropTypes.array.isRequired,
+  isCurrencyDisabled: PropTypes.bool.isRequired,
+  className: PropTypes.string,
+};
+export default InputComponent;
+
